524b2a08caeccbfd7cbf45b60144ac5d46de5eac
yongwookkkkim <99084710+yongwookkkkim@users.noreply.github.com>
2022-05-24 22:51:33 +0900

    numpy tutorial unsaved parts upload

--- learn/python/numpy/numpy_tutorial.py
+++ learn/python/numpy/numpy_tutorial.py
@@ -1,59 +1,59 @@
 import numpy as np
-
-#a=np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])
-#b=np.array([1,2,3,4,5,6], dtype='int8') #you can customise the type of the array
-#c=np.array([[[1,2], [3,4]], [[5,6], [7,8]]])
+'''
+a=np.array([[9.0, 8.0, 7.0], [6.0, 5.0, 4.0]])
+b=np.array([1,2,3,4,5,6], dtype='int8') you can customise the type of the array
+c=np.array([[[1,2], [3,4]], [[5,6], [7,8]]])
 
 #get dimension
-#print(a.ndim)
+print(a.ndim)
 
 #get shape
-#print(a.shape)
+print(a.shape)
 
 #get type
-#print(a.dtype)
+print(a.dtype)
 
 #the number of bytes each item takes up
-#print(b.itemsize)
+print(b.itemsize)
 
 #the number of bytes the array takes up
-#print(a.nbytes)
+print(a.nbytes)
 
 #get a specific element (wokrds outside in)
-#print(a[1,:]) # negative index also possible
+print(a[1,:])  #negative index also possible
 #[startindex:endindex:stepsize]
 
 #change an element
-#a[1,2]=20.0
-#a[:,2]=5 # or [1.0, 2.0]
+a[1,2]=20.0
+a[:,2]=5  #or [1.0, 2.0]
 
 #all 0s matrix
-#zeros=np.zeros((5,5,5)) #or a single number tossed in
+zeros=np.zeros((5,5,5)) #or a single number tossed in
 
 #all 1s matrix
-#onees=np.ones((4,2,2), dtype='int8')
+onees=np.ones((4,2,2), dtype='int8')
 
 #any other number
-#s99=np.full((2,2), 99) # the dimension and the number you want to fill it with
+s99=np.full((2,2), 99)  #the dimension and the number you want to fill it with
 
 #same dimension as another array
-#samedim=np.full_like(c, 6)
+samedim=np.full_like(c, 6)
 
 #random decimal numbers
-#randoms=np.random.rand(4,2) # not a tuple!
+randoms=np.random.rand(4,2)  #not a tuple!
 
 #random data, same dimension
-#randsamedim=np.random.random_sample(c.shape)
+randsamedim=np.random.random_sample(c.shape)
 
 #random integer values
-#randints=np.random.randint(4,7,size=(3,3)) # random int between 4 and 7
+randints=np.random.randint(4,7,size=(3,3))  #random int between 4 and 7
 
 #identity matrix
-#eyes=np.identity(5)
+eyes=np.identity(5)
 
 #repeating another array
-#arr=np.array([[1,2,3]])
-#r1=np.repeat(arr, 3, axis=0)
+arr=np.array([[1,2,3]])
+r1=np.repeat(arr, 3, axis=0)
 
 #practice
 # 1 1 1 1 1
@@ -61,42 +61,59 @@ import numpy as np
 # 1 0 9 0 1
 # 1 0 0 0 1
 # 1 1 1 1 1
-#base=np.full((5,5), 1)
-#base[1:-1, 1:-1]=0
-#base[2,2]=9
+base=np.full((5,5), 1)
+base[1:-1, 1:-1]=0
+base[2,2]=9
 
 #deep and shallow copy
-#x=np.array([1,2,3])
-#y=x #this creates a shallow copy
-#y=x.copy() #this creates a deep copy
+x=np.array([1,2,3])
+y=x #this creates a shallow copy
+y=x.copy() #this creates a deep copy
 
 #element-wise operations
-#x=np.array([1,2,3,4])
-#x=x+2
-#x=np.sin(x) # all of these result in element-wise operations
+x=np.array([1,2,3,4])
+x=x+2
+x=np.sin(x)  #all of these result in element-wise operations
 
 #linear algebra
-#a=np.ones((2,3))
-#b=np.full((3,2),2)
-#print(np.matmul(a,b)) # matrix multiplication
+a=np.ones((2,3))
+b=np.full((3,2),2)
+print(np.matmul(a,b))  #matrix multiplication
 
 #determinant
-#c=np.identity(3)
-#print(np.linalg.det(c))
-#eigenvalue, inverse of matrix, etc are available
+c=np.identity(3)
+print(np.linalg.det(c))
+eigenvalue, #inverse of matrix, etc are available
 
 #statistics
-#stats=np.array([[1,2,3], [4,5,6]])
-#print(np.min(stats), axis=0) #maximum, axis=0 to print mins of each column
-#print(np.min(stats)) #minimum
-#print(np.sum(stats)) #sum
+stats=np.array([[1,2,3], [4,5,6]])
+print(np.min(stats), axis=0) #maximum, axis=0 to print mins of each column
+print(np.min(stats)) #minimum
+print(np.sum(stats)) #sum
 
 #reorganising arrays
-#before=np.array([[1,2,3,4],[5,6,7,8]])
-#after=before.reshape((8,1))
+before=np.array([[1,2,3,4],[5,6,7,8]])
+after=before.reshape((8,1))
 
 #vertically stacking vector
 v1=np.array([9,8,7,6])
 v2=np.array([1,2,3,4])
+print(np.vstack([v1,v2])) #horizontal stack (very similar) exists
 
-print(np.vstack([v1,v2]))
+#loading data from a txt file
+filedata=np.genfromtxt('filename.txt', delimiter=', ') 
+filedata=filedata.astype('int32') #filedata.astype() alone only makes a copy
+
+#advanced indexing and boolean masking
+print(filedata>50) -> gives a matrix of same dimension, each item of the value (filedata>50)
+filedata[filedata>50] -> only the values above 50 are displayed
+
+#you can index with a list in numpy
+a=np.array([1,2,3,4,5,6,7,8])
+print(a[[1,2,8]])
+
+np.any(filedata>50, axis=0) #whether any of the item each column are above 50
+np.all(filedata>50, axis=0) #whether all of the item each column are above 50
+
+print(((filedata>50) & (filedata<100)))
+'''
